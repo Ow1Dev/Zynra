1e2166dd07ff796c85830cc807823e56a423a822
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-06-04 23:26:30 +0200

    feat: remove vorpal and adding nix config

---
+++ flake.nix
@@ -0,0 +1,45 @@
+{
+  description = "Configuration for Zynra";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: let
+        name = "Zynra";
+        vendorHash = "sha256-nZu9e6jKHZ2ndhWCjIbrGjy7qRBOsdrteM10btV6+qo="; # update whenever go.mod changes
+      in {
+        devShells = {
+          default = pkgs.mkShell {
+            inputsFrom = [self'.packages.default];
+            buildInputs = with pkgs; [
+              go_1_24
+            ];
+          };
+        };
+
+        packages = {
+          default = pkgs.buildGoModule {
+            inherit name vendorHash;
+            src = ./.;
+            subPackages = ["cmd"];
+            postBuild = ''
+              mv $GOPATH/bin/cmd $GOPATH/bin/zyndra
+            '';
+          };
+        };
+      };
+    };
+}
---
+++ justfile
@@ -0,0 +1,6 @@
+package profile='default':
+    nix build \
+        --json \
+        --no-link \
+        --print-build-logs \
+        '.#{{ profile }}'
