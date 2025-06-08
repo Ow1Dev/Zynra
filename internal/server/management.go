f1e6d19dfa0b001d0b768a2e6a718491a3d7ec23
Mads Bruun Jensen <31339803+Ow1Dev@users.noreply.github.com>
2025-06-08 20:15:23 +0200

    feat: error if it cannot find the ip

--- internal/server/management.go
+++ internal/server/management.go
@@ -7,7 +7,9 @@ import (
 	pb "github.com/Ow1Dev/Zynra/pkgs/api/managment"
 	"github.com/rs/zerolog/log"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/status"
 )
 
 type managementServiceServer struct {
@@ -26,6 +28,7 @@ func (m *managementServiceServer) Connect(ctx context.Context, request *pb.Conne
 		log.Info().Msgf("Received connection request from %s:%d", clientIP, request.GetPort())
 	} else {
 		log.Error().Msg("Failed to get peer information from context")
+		return nil, status.Errorf(codes.Internal, "failed to get peer information from context")
 	}
 
 	return &pb.ConnectResponse{
